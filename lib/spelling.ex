f1763e81eb5b07ea64e36d4b148e49845746bebb
Patrick Gombert <patrickgombert@gmail.com>
2013-07-08 14:46:05 -0500

    Partially finished known_edits

--- lib/spelling.ex
+++ lib/spelling.ex
@@ -25,8 +25,40 @@ defmodule Spelling do
     text
   end
 
+  def edits1(word) do
+    length = String.length(word)
+    range = 1..(length)
+    s = Enum.map(range, fn(index) ->
+      {String.slice(word, 0, index), String.slice(word, index, length)}
+    end)
+    deletes = Enum.filter_map(s,
+                fn({_, b}) -> b != "" end,
+                fn({a, b}) -> "#{a}#{String.slice(b, 1, String.length(b))}" end)
+    transposes = Enum.filter_map(s,
+                fn({_, b}) -> String.length(b) > 1 end,
+                fn({a, b}) -> "#{a}#{String.slice(b, 1, 2)}#{String.slice(b, 0, 1)}#{String.slice(b,2,String.length(b))}" end)
+    replaces = Enum.filter_map(s,
+                fn({_, b}) -> b != "" end,
+                fn({a, b}) -> Enum.map(alphabet, fn(c) ->
+                                "#{a}#{c}#{String.slice(b,1,String.length(b))}"
+                              end)
+               end)
+    inserts = Enum.map(s, fn({a, b}) -> Enum.map(alphabet, fn(c) ->
+                                          "#{a}#{b}#{c}"
+                                       end
+                          end)
+    deletes ++ transposes ++ replaces ++ inserts # Might need to be a set
+  end
+
+  def known_edits2(word) do
+    
+  end
+
   def correct(input) do
     "access"
   end
 
+  defp alphabet do
+    "abcdefghijklmnopqrstuvwxyz"
+  end
 end
