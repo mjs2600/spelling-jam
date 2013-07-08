e99b82dcf9f330bb39e914c09da82299c340b2ab
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-07-08 14:43:00 -0500

    get count

--- lib/spelling.ex
+++ lib/spelling.ex
@@ -1,9 +1,32 @@
 defmodule Spelling do
+  def words(text) do
+    Regex.split(%r/[^a-z]+/, text)
+  end
+
   def train(features) do
-    
+    train features, HashDict.new
+  end
+
+  def train([], acc) do
+    acc
+  end
+
+  def train(features, acc) do
+    [ h | t ] = features
+    current_count = acc[h]
+    new_count = (current_count || 0) + 1
+    train t, Dict.put(acc, h, new_count)
+  end
+
+  def nwords, do: file |> words |> train
+
+  defp file do
+    {:ok, text} = File.read("data/big.txt")
+    text
   end
 
   def correct(input) do
     "access"
   end
+
 end
